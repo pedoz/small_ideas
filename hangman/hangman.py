db1b0d3dac3930e19bc68dae56a1cf51e038d004
somosasuke <l.felipepedo@hotmail.com>
2023-04-10 12:08:11 -0300

    testing

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,41 +1,60 @@
 import random
-import re
+import csv
 
 
 def main():
     word = get_word()
     print(hangman(word))
 
-
 def get_word():
-    word_list = [
-            "saude",
-            "pianos",
-            "casa",
-    ]
+    word_list = []
+    try:
+        nivel = input("Qual a dificuldade? ")
+        if int(nivel) == 0:
+            with open('palavras/alea.csv') as file:
+                for line in file:
+                    word_list.append(line)
+        elif int(nivel) == 1:
+            with open('facil.csv') as file:
+                for line in file:
+                    word_list.append(line)
+        elif int(nivel) == 2:
+            with open('palavras/medio.csv') as file:
+                for line in file:
+                    word_list.append(line)
+        elif int(nivel) == 3:
+            with open('palavras/dificil.csv') as file:
+                for line in file:
+                    word_list.append(line)
+        else:
+            print('Escolha um desses números: 0 - 1 - 2 - 3')
+            get_word()
+    except EOFError:
+        get_word()
+
 
     sel_word =  random.choice(word_list)
     return list(sel_word)
-
 
 
 def hangman(word):
     letters = word
     count = len(word)
     checker = list()
-    n = 5
+    n = 6
     wrong_l = list()
 
     for _ in letters:
-        print("_", end=' ')
         checker.append("_")
-    print(checker, end ='\n')
+    print(*checker, sep = ' ')
 
     while True:
 
         if count == 0:
-            return 'Parabens'
+            return 'Parabéns'
         if n == 0:
+            get_man(n)
+            print(word.upper())
             return "Falhou :("
         
         letra = input("Letra:")
@@ -44,11 +63,11 @@ def hangman(word):
             print("Invalido")
         elif letra.isnumeric():
             print("Invalido")
-        elif letra in checker:
+        elif letra.upper() in checker:
             print('Letra repetida')
         elif letra in wrong_l:
             print('Já não tem essas letras: ', end = '')
-            print(wrong_l)
+            print(*wrong_l, sep = ', ')
 
         else:
             try:
@@ -57,21 +76,81 @@ def hangman(word):
                     if letters[i] == letra:
                         count = count - 1
                         checker.pop(i)
-                        checker.insert(i, letra)
+                        checker.insert(i, letra.upper())
                         j = j + 1
                     elif ((i + 1) == len(letters)) and (j == 0):
                         n = n - 1
                         wrong_l.append(letra)
-                        print("letras que não tem:", end = ' ')
-                        for i in wrong_l:
-                            print(i, sep = ', ')
-
-                print(checker)
+                        print(f"\nletras que não tem:", end = ' ')
+                        print(*wrong_l, sep = ', ')
+                if n == 0:
+                    pass
+                else:
+                    get_man(n)
+                    print(*checker, sep = ' ')
+                    print('')
 
             except IndexError:
                 print("erro")
 
 
+def get_man(erros_restantes):
+    if erros_restantes == 0:
+                return print('''\
+   ______________
+   |             |
+   |             X
+   |            /|\\
+   |            / \\
+  /T\\                 ''', end = '')
+    elif erros_restantes == 6:
+                return print('''\
+   ______________
+   |             |
+   |             O
+   |            
+   |            
+  /T\\                 ''', end = '')
+    elif erros_restantes == 5:
+                return print('''\
+   ______________
+   |             |
+   |             O
+   |             |
+   |            
+  /T\\                 ''', end = '')
+    elif erros_restantes == 4:
+                return print('''\
+   ______________
+   |             |
+   |             O
+   |            /|
+   |            
+  /T\\                 ''', end = '')
+    elif erros_restantes == 3:
+                return print('''\
+   ______________
+   |             |
+   |             O
+   |            /|\\
+   |            
+  /T\\                 ''', end = '')
+    elif erros_restantes == 2:
+                return print('''\
+   ______________
+   |             |
+   |             O
+   |            /|\\
+   |            /
+  /T\\                 ''', end = '')
+    elif erros_restantes == 1:
+        return print('''\
+   ______________
+   |             |
+   |             O
+   |            /|\\
+   |            / \\
+  /T\\                 ''', end = '')
 
 
 
--- hangman/hangman_basic_done.py
+++ hangman/hangman_basic_done.py
@@ -4,14 +4,13 @@ import re
 
 def main():
     word = get_word()
-    #word agr é uma lista
     print(hangman(word))
 
 
 def get_word():
     word_list = [
-            "saude",
-            "pianos",
+            #"saude",
+            #"pianos",
             "casa",
     ]
 
@@ -22,54 +21,56 @@ def get_word():
 
 def hangman(word):
     letters = word
+    count = len(word)
     checker = list()
     n = 5
-    wrong_l = list
+    wrong_l = list()
 
     for _ in letters:
         print("_", end=' ')
         checker.append("_")
+    print(checker, end ='\n')
 
-    while n >= 0:
+    while True:
 
-        if len(word) == 0:
+        if count == 0:
             return 'Parabens'
+        if n == 0:
+            return "Falhou :("
         
         letra = input("Letra:")
-        print(word)
-        print(letra)
 
         if len(letra) != 1:
             print("Invalido")
         elif letra.isnumeric():
             print("Invalido")
-        
+        elif letra in checker:
+            print('Letra repetida')
+        elif letra in wrong_l:
+            print('Já não tem essas letras: ', end = '')
+            print(wrong_l)
+
         else:
             try:
                 j = 0
                 for i in range(len(letters)):
                     if letters[i] == letra:
-                        word.remove(i)
+                        count = count - 1
                         checker.pop(i)
                         checker.insert(i, letra)
                         j = j + 1
                     elif ((i + 1) == len(letters)) and (j == 0):
                         n = n - 1
                         wrong_l.append(letra)
-                        print("Nâo contém essa letra")
-                        print("letras que não tem:" + wrong_l, sep = " ")
+                        print("letras que não tem:", end = ' ')
+                        print(*wrong_l, sep = ', ')
 
                 print(checker)
 
             except IndexError:
                 print("erro")
 
-    for i in range(len(checker)):
-        if checker[i] == "_":
-            return "Não conseguiu :("
 
-    else:
-        return "Congrats"
 
 
 
